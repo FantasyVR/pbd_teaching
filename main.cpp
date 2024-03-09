ae2d107f2d297c22d74eff3c05312fc561424f13
unknown <y2505418927@gmail.com>
2024-03-09 14:24:49 +0800

    rod display

--- main.cpp
+++ main.cpp
@@ -3,32 +3,20 @@
 int main(int argc, char *argv[])
 {
   // Inline mesh of a cube
-  const Eigen::MatrixXd V= (Eigen::MatrixXd(8,3)<<
+  const Eigen::MatrixXd V= (Eigen::MatrixXd(3,3)<<
     0.0,0.0,0.0,
-    0.0,0.0,1.0,
-    0.0,1.0,0.0,
-    0.0,1.0,1.0,
-    1.0,0.0,0.0,
-    1.0,0.0,1.0,
-    1.0,1.0,0.0,
-    1.0,1.0,1.0).finished();
-  const Eigen::MatrixXi F = (Eigen::MatrixXi(12,3)<<
-    0,6,4,
-    0,2,6,
-    0,3,2,
-    0,1,3,
-    2,7,6,
-    2,3,7,
-    4,6,7,
-    4,7,5,
-    0,4,5,
-    0,5,1,
-    1,5,7,
-    1,7,3).finished();
+	1.0,0.0,0.0,
+    2.0,0.0,0.0).finished();
+  const Eigen::MatrixXd C = (Eigen::MatrixXd(1, 3) <<
+      1.0, 0.0, 0.0).finished();
+  const Eigen::MatrixXi E = (Eigen::MatrixXi(2, 2) <<
+     0, 1,
+     1, 2
+      ).finished();
 
   // Plot the mesh
   igl::opengl::glfw::Viewer viewer;
-  viewer.data().set_mesh(V, F);
-  viewer.data().set_face_based(true);
+  viewer.data().set_points(V, C);
+  viewer.data().set_edges(V, E, C);
   viewer.launch();
 }
